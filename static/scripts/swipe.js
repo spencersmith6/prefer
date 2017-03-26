855a80d76211c4c4d328d6ad3ea6e292e58c0ff3
Kelsey MacMillan <kelsey.macmillan@gmail.com>
2017-03-26 05:23:04 -0700

    add animation for swipe. It's not the most beautiful thing ever... but it mostly works.

--- static/scripts/swipe.js
+++ static/scripts/swipe.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+    // submit user preference then get and load new product
     function submitPreference(preferenceValue){
         $.ajax({
             method: 'POST',
@@ -7,20 +8,23 @@ $(document).ready(function(){
             data: {
                 product_id: $('#product-id').text(),
                 preference: preferenceValue
+            },
+            cache: false,
+            success: function(newProduct){
+                $('#product-id').text(newProduct.product_id)
+                $('#product-name').text(newProduct.product_name)
+                $('#product-img').attr("src", newProduct.product_image)
+                $("#product-container").animate({'right':'0px'}, 0).animate({'opacity': '1.0'}, 100) // reset image position
             }
-        }).done(function(newProduct){
-            $('#product-id').text(newProduct.product_id)
-            $('#product-name').text(newProduct.product_name)
-            $('#product-img').attr("src", newProduct.product_image)
-        }).fail(function(){
-            console.log('failed') // debug statement
         })
     }
 
+    // wrapper function for case where user clicks button
     function submitPreferenceClick(event){
         submitPreference($(event.target).text())
     }
 
+    // wrapper function for case where user swipes
     function submitPreferenceSwipe(event){
         if (event.type == 'swiperight'){
             submitPreference('Like')
@@ -30,11 +34,28 @@ $(document).ready(function(){
         }
     }
 
-    $('.preference-button').click(submitPreferenceClick)
 
-    // prevent browser from dragging image during swipe
-    $('#product-img').on('dragstart', function(event) {event.preventDefault()})
-    $('#product-img').on('swipeleft', submitPreferenceSwipe)
-    $('#product-img').on('swiperight', submitPreferenceSwipe)
+    // on click, do the appropriate swipe animation, then call the ajax function
+    $('.preference-button').click(function(event) {
+        if ($(this).text() == 'Like'){
+            $("#product-container").animate({'right':'-500px', 'opacity': '0.0'}, 500,'swing',
+                                    function(){submitPreferenceClick(event)})
+        }
+        else if ($(this).text() == 'Dislike'){
+            $("#product-container").animate({'right':'500px', 'opacity': '0.0'}, 500,'swing',
+                                    function(){submitPreferenceClick(event)})
+        }
+    })
+
+    // on swipe, do the appropriate swipe animation, then call the ajax function
+    $('#product-img').on('dragstart', function(event) {event.preventDefault()}) // prevent browser from dragging image
+
+    $('#product-img').on('swiperight', function(event) {
+        $("#product-container").animate({'right':'-500px', 'opacity': '0.0'}, 500,'swing', function(){submitPreferenceSwipe(event)})
+    })
+
+        $('#product-img').on('swipeleft', function(event) {
+        $("#product-container").animate({'right':'500px', 'opacity': '0.0'}, 500,'swing', function(){submitPreferenceSwipe(event)})
+    })
 
 })
